eb325d9121ba7d15279847f9adb0d917d69ae7ef
Walker Davis <walker@wkeithdavis.com>
2019-03-05 21:47:53 -0500

    Added fade-in and splash screen

---
+++ scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.welcome').css('display', 'none');
+  $('.welcome').fadeIn(1000);
+  $('.welcome').delay(1000).fadeOut(1000);
+  $('.titleText').css('display', 'none');
+  $('.buttons').css('display', 'none');
+  $('.titleText').delay(3000).fadeIn(2000);
+  $('.buttons').delay(3000).fadeIn(3000);
+});
